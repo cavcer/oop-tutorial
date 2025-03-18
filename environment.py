2bd07dedb0cfb96ea5b6e52a83e81c4eb9c6ac0c
Kevser <akdagk24@itu.edu.tr>
2025-03-18 23:30:29 +0300

    env

---
+++ environment.py
@@ -0,0 +1,19 @@
+class Environment:
+
+    def __init__(self,drone_list)
+        self.drone_list = []
+
+    def add_drone(self, drone, drone_list):
+        self.drone_list.append(drone)
+        print("drone added")
+
+    def list_drones(self):
+        print("Active İHAs:")
+        for drone in self.drones:
+            print(f"{self.drone.__name__}") 
+
+   
+
+
+
+
